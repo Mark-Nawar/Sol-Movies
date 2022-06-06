54e53d52dda69e1b277c3611da56730e7e40fe7b
Azorji Kelechi Oliver <kelechioliver96@gmail.com>
2022-06-06 09:24:02 +0100

    add key for shop pub ke

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 import { PublicKey } from '@solana/web3.js';
 
 const shopAddress = process.env.REACT_APP_SHOP_PUB_KEY;
+console.log(shopAddress)
 export const SHOP_ADDRESS = new PublicKey(shopAddress);
